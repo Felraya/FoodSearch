0a954e26ebce869e8650167c52f04f4bd7b2ef42
Baptiste <baptiste.boursin@gmail.com>
2020-03-24 11:21:09 +0100

    modif class resultats

--- app/src/main/java/com/example/mini_projet/Resultats.java
+++ app/src/main/java/com/example/mini_projet/Resultats.java
@@ -1,14 +1,29 @@
 package com.example.mini_projet;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Resultats extends AppCompatActivity {
 
+    ListView lv;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultats);
+
+    lv = findViewById(R.id.results);
+
+    lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        }
+    });
     }
+
+
 }
