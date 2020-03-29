20fbf6eabc99a0d42ed2a0513f2100a5bbefae77
Théo Grollier <theo.grollier85@gmail.com>
2020-03-29 18:27:15 +0200

    Bonne version

--- app/src/main/java/com/example/mini_projet/ListGenre.java
+++ app/src/main/java/com/example/mini_projet/ListGenre.java
@@ -4,14 +4,35 @@ package com.example.mini_projet;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.*;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.content.Intent;
+
+import android.widget.SeekBar;
+import android.widget.Toast;
+import android.widget.Spinner;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.stream.JsonToken;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONTokener;
 
 
 public class ListGenre extends AppCompatActivity {
@@ -24,14 +45,12 @@ public class ListGenre extends AppCompatActivity {
     private int itemSeekBar;
     private String keyWord;
     private String apiKey = "f4191411045d45d0aed31c69627d5499";
-    private TextView nbsearch;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         resultats = findViewById(R.id.seekBar);
-        nbsearch = findViewById(R.id.NbSearch);
         spinner = findViewById(R.id.spinner);
         bouton = findViewById(R.id.rechercher);
         search = findViewById(R.id.search);
@@ -55,8 +74,6 @@ public class ListGenre extends AppCompatActivity {
 
         spinner.setAdapter(aa);
 
-        nbsearch.setText("0");
-
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -73,7 +90,6 @@ public class ListGenre extends AppCompatActivity {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 itemSeekBar = progress;
-                nbsearch.setText(String.valueOf(progress));
             }
 
             @Override
@@ -90,20 +106,38 @@ public class ListGenre extends AppCompatActivity {
         bouton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v){
                 keyWord = search.getText().toString();
-            }
-        });
+                //String urlJson = "https://api.spoonacular.com/recipes/search?query="+keyWord+"&cuisine="+itemSpinner+"&number="+itemSeekBar+"&apiKey="+apiKey;
+                Intent i = new Intent(ListGenre.this, Resultats.class);
+                Ion.with(v.getContext())
+                        .load("https://api.spoonacular.com/recipes/search?query="+keyWord+"&cuisine="+itemSpinner+"&number="+itemSeekBar+"&apiKey="+apiKey)
+                        .setLogging("ION_LOGS", Log.DEBUG)
+                        .asJsonObject()
+                        .setCallback(new FutureCallback<JsonObject>() {
+                            @Override
+                            public void onCompleted(Exception e, JsonObject result) {
+                                // do stuff with the result of error
+                                ArrayList<String> list = new ArrayList<>();
+                                JsonArray res = result.get("results").getAsJsonArray();
+                                for(int i = 0; i < res.size(); i++){
+                                    list.add(res.get(i).getAsJsonObject().get("title").getAsString());
+                                }
+                                System.out.println(list);
+
+                            }
+
+                            });
+                        }
+            });
 
 
 
-        String jsonText = "https://api.spoonacular.com/recipes/search?query="+keyWord+"&cuisine="+itemSpinner+"&number="+itemSeekBar+"&apiKey=" + apiKey;
-        //https://api.spoonacular.com/recipes/search?query=cheese&cuisine=Nordic&number=10&apiKey=f4191411045d45d0aed31c69627d5499
-        //info par id :
-        //https://api.spoonacular.com/recipes/{id}/information?apiKey=f4191411045d45d0aed31c69627d5499
-        //130953
 
-        //JSONObject json = JSONReader.readJsonFromUrl("https://api.spoonacular.com/recipes/search?query=cheese&cuisine=italian&number=3&apiKey=" + apiKey);
 
         }
+
+
+
+
 
 
 }
